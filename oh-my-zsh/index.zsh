e4c492987780fdab6cca79934317ae2be017154e
rvrheenen <rick.van.rheenen@gmail.com>
2018-05-30 22:43:04 +0200

    zsh config

--- oh-my-zsh/index.zsh
+++ oh-my-zsh/index.zsh
@@ -1,9 +1,9 @@
 export ZSH=~/.oh-my-zsh
 
-ZSH_THEME="awesomepanda"
+ZSH_THEME="gnzh"
 DEFAULT_USER=$(whoami)
 
-plugins=(git)
+plugins=(colored-man-pages colorize cp git tmux)
 
 source $ZSH/oh-my-zsh.sh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- oh-my-zsh/install.zsh
+++ oh-my-zsh/install.zsh
@@ -3,3 +3,10 @@ export ZSH=~/.oh-my-zsh
 if [ ! -d $ZSH ]; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git $ZSH
 fi
+
+# if [ -d $ZSH ]; then
+#   if [ ! -f $ZSH/custom/themes/bullet-train.zsh-theme ]; then
+#     wget "https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme" -P $ZSH/custom/themes/
+#     # git clone https://github.com/bhilburn/powerlevel9k.git $ZSH/custom/themes/powerlevel9k
+#   fi
+# fi
